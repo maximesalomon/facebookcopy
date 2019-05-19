911fdef8fc9bd47c6b187594e2f2292e61b39b99
Maxime Salomon <maxime@croissant.io>
2019-05-19 18:45:36 +0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home = () => {
+  return <h1>React + React Router + Redux Starter Kit</h1>;
+}
+
+const About = () => {
+  return <h1>About</h1>;
+}
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>React + React Router + Redux Starter Kit</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/about/" component={About} />
+      </div>
+    </Router>
   );
 }
 
