e029ee852370787a6be67019361cbfe2efbb7c58
Maxime Salomon <maxime@croissant.io>
2019-05-19 21:44:00 +0200

    Add compose

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/rootReducer';
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer,
+    compose(
+        applyMiddleware(thunk)
+    )
+);
 
 ReactDOM.render(
 <Provider store={store}>
